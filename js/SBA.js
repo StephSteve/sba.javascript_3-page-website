169beb25ea3124c2040ddda7b761a6e6e3e3984e
StephSteve <stephanie.oncallbusiness@gmail.com>
2020-07-10 13:15:15 -0400

    SBA Updates

--- js/SBA.js
+++ js/SBA.js
@@ -1,8 +1,9 @@
-document.getElementById("joinbtn").addEventListener("click", function(){
+.addEventListener("click", function(){
+    let joinbtn = document.getElementById("joinbtn");
     document.getElementById("newMessage").innerHTML = "Thank You!";
 });
-
-if() {
-    
+let p1 = document.getElementById("p1");
+while (p1.innerHTML.length < 74 ) {
+    let p1 = document.getElementById("p1");
 
 }
